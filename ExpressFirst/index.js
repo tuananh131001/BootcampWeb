77022b3f1c0b9e4d8c82578f7b133d98f298abe1
William <s3864077@rmit.edu.vn>
2022-05-25 21:38:10 +0700

    qua du

--- ExpressFirst/index.js
+++ ExpressFirst/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use(() => console.log("We got request"));
+app.set("view engine", "ejs");
+
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos reponse")
+});
+
+app.post("/tacos", (req, res) => { 
+  res.send("POST ")
+})
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
